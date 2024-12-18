dedba86ee98983866d35ea8b1ebe372933734ac6
NikolaySkotnikov <Ocean3001@yandex.ru>
2024-12-18 19:52:19 +0300

    изменение settinds

--- oee/settings.py
+++ oee/settings.py
@@ -131,9 +131,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-SECURE_SSL_REDIRECT = True
-SECURE_SSL_REDIRECT = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
