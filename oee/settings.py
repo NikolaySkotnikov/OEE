70e277537005957f99f17e485358887e1cb5d668
NikolaySkotnikov <Ocean3001@yandex.ru>
2024-12-18 14:02:23 +0300

    ALLOWED_HOSTS

--- oee/settings.py
+++ oee/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['147.45.164.9', 'localhost', '127.0.0.1']
 
 
 # Application definition
