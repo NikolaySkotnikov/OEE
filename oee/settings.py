05421318037c994812c0dad23206c3e8a3418866
NikolaySkotnikov <Ocean3001@yandex.ru>
2024-12-30 04:04:17 +0300

    6

--- oee/settings.py
+++ oee/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'oee-stat.ru', 'www.oee-stat.ru', '85.193.81.230']
+ALLOWED_HOSTS = ['85.193.81.230', 'oee-stat.ru', 'www.oee-stat.ru']
 
 # Application definition
 
