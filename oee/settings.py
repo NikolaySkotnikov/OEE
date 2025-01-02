de8ebdbede80879652e2cf9d96bf31e05c0073a9
NikolaySkotnikov <Ocean3001@yandex.ru>
2025-01-02 19:42:52 +0300

    atatic

--- oee/settings.py
+++ oee/settings.py
@@ -129,11 +129,11 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = '/home/v/OEE/static'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/v/OEE/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
