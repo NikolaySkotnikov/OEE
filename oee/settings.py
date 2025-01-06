b0cab24babff2d009da109ff1670975be95bc33d
NikolaySkotnikov <Ocean3001@yandex.ru>
2025-01-06 14:20:18 +0300

    статика

--- oee/settings.py
+++ oee/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/v/OEE/staticfiles'
+STATIC_ROOT = '/home/v/OEE/static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/home/v/OEE/media'
 
